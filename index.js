484555d67d0902f83e26ae5be80cea3a83681f11
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-08 19:00:45 -0300

    adicionar const video, canvas, photo

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const captureBtn = document.getElementById('captureBtn');
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const photo = document.getElementById('photo');
+
