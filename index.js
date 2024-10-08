acac5962711fae7903e8957fbe423c09bb1922f8
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-08 19:08:07 -0300

    adicionar function p acessar camera na L7

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const photo = document.getElementById('photo');
 
+// Função para acessar a câmera
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(err => {
+        console.error("Erro ao acessar a câmera: ", err);
+    });
